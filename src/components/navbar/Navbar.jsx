4151de3441c455d43fc7ff1928c0dc63d435d333
Sai Chandan <saichandhan.yata44@gmail.com>
2023-09-30 02:22:59 +0530

    CRD operations completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,63 +1,67 @@
 "use client";
 
-import React from 'react'
-import Link from 'next/link';
-import styles from './navbar.module.css';
-import DarkModeToggle from '../darkModeToggle/DarkModeToggle';
+import Link from "next/link";
+import React from "react";
+import styles from "./navbar.module.css";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
+import { signOut, useSession } from "next-auth/react";
 
-function Navbar() {
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 
-    const links = [
-        {
-          id: 1,
-          title: "Home",
-          url: "/",
-        },
-        {
-          id: 2,
-          title: "Portfolio",
-          url: "/portfolio",
-        },
-        {
-          id: 3,
-          title: "Blog",
-          url: "/blog",
-        },
-        {
-          id: 4,
-          title: "About",
-          url: "/about",
-        },
-        {
-          id: 5,
-          title: "Contact",
-          url: "/contact",
-        },
-        {
-          id: 6,
-          title: "Dashboard",
-          url: "/dashboard",
-        },
-      ];
+const Navbar = () => {
+  const session = useSession();
 
   return (
     <div className={styles.container}>
-    <Link href="/" className={styles.logo}>
-      S-book
-    </Link>
-    <div className={styles.links}>
-      <DarkModeToggle />
-      {links.map((link) => (
-        <Link key={link.id} href={link.url} className={styles.link}>
-          {link.title}
-        </Link>
-      ))}
-      
-        <button className={styles.logout} onClick={() => alert('logged out!!') } > Logout </button>
-
+      <Link href="/" className={styles.logo}>
+        S-Book
+      </Link>
+      <div className={styles.links}>
+        <DarkModeToggle />
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
+        ))}
+        {session.status === "authenticated" && (
+          <button className={styles.logout} onClick={signOut}>
+            Logout
+          </button>
+        )}
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default Navbar;
